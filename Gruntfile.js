ba6e6000b30766e4362bb18edb4f4edf440c5dd4
Aditya Kumar <aditya.podia@gmail.com>
2017-09-14 00:54:45 +0530

    feat(akp-devnotes): implemented consumption of this plugin

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+      akp_devnotes: {
+          options: {
+            dest: 'destination/paterns-par',
+            excludeComponents: ['devNotes', 'message', 'position', 'transition', 'utilities'],
+            partialsTemplate:'app/devnotes-template.html'
+          },
+          files: {
+            'dest/default_options': ['src/srcfile', 'src/123'],
+          }
+        },
+      });
+    grunt.registerTask('default', ['akp_devnotes']);
+    grunt.loadNpmTasks('grunt-template');
+    grunt.loadNpmTasks('grunt-akp-devnotes');
+}
